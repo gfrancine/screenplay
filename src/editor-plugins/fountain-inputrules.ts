a2dcd0c2be2db658b40201f6599e002c0c0ee361
gfrancine <gracefrancine.t@gmail.com>
2021-12-15 21:37:12 +0700

    fix replacingInputRule adding extra line breaks

--- src/editor-plugins/fountain-inputrules.ts
+++ src/editor-plugins/fountain-inputrules.ts
@@ -1,13 +1,23 @@
 import { inputRules, InputRule } from "prosemirror-inputrules";
 import { NodeType, MarkType } from "prosemirror-model";
+import { Transaction } from "prosemirror-state";
 import { schema } from "../editor";
 import { isBetween } from "../internal-util/prosemirror";
 
 // replace the current node with a new blank node on input rule match
 function replacingInputRule(pattern: RegExp, nodeType: NodeType) {
-  return new InputRule(pattern, (state, _matches, start, end) => {
-    if (!isBetween("action", state)) return null;
-    return state.tr.replaceWith(start, end, nodeType.createAndFill());
+  return new InputRule(pattern, (state, _matches) => {
+    let tr: Transaction | null = null;
+
+    if (!state.selection.empty) return tr;
+    const { from } = state.selection;
+    state.doc.nodesBetween(from, from, (node, start) => {
+      if (node.type.name !== "action") return;
+      const end = start + node.nodeSize;
+      tr = state.tr.replaceWith(start, end, nodeType.create()).scrollIntoView();
+    });
+
+    return tr;
   });
 }
 
@@ -29,7 +39,7 @@ function markInputRule(regexp: RegExp, markType: MarkType, matchNumber = 1) {
 export const fountainInputRulesPlugin = inputRules({
   rules: [
     replacingInputRule(/^> $/, schema.nodes.transition),
-    replacingInputRule(/^. $/, schema.nodes.scene),
+    replacingInputRule(/^\. $/, schema.nodes.scene),
     replacingInputRule(/^@ $/, schema.nodes.character),
     markInputRule(/\*\*([^*]+)\*\*/, schema.marks.strong),
     markInputRule(/(?:^|[^*])\*([^*]+)\*/, schema.marks.em, 2),
